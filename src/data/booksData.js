639e8133c1a4f46dc8ae03bd39cd5b38bc0cc856
Hana Stupková <hstupkova@gmail.com>
2021-12-06 17:27:23 +0100

    dummy data

---
+++ src/data/booksData.js
@@ -0,0 +1,64 @@
+const booksData = [
+  {
+    id: 1,
+    title: "The Last Unicorn",
+    author: "Peter S. Beagle",
+    gender: "male",
+    country: "USA",
+    pages: 218,
+    published: 1968,
+    category: "fiction",
+    genre: "fantasy",
+    link: "https://www.goodreads.com/book/show/45153348-the-last-unicorn"
+  },
+  {
+    id: 2,
+    title: "Tournament of Losers",
+    author: "Megan Derr",
+    gender: "female",
+    country: "USA",
+    pages: 248,
+    published: 2015,
+    category: "fiction",
+    genre: "gay romance",
+    link: "https://www.goodreads.com/book/show/28862347-tournament-of-losers"
+  },
+  {
+    id: 3,
+    title: "Saturnin",
+    author: "Zdeněk Jirotka",
+    gender: "male",
+    country: "Czech",
+    pages: 250,
+    published: 1942,
+    category: "fiction",
+    genre: "humor",
+    link: "https://www.goodreads.com/book/show/57960558-saturnin"
+  },
+  {
+    id: 4,
+    title: "Meč osudu",
+    author: "Andrzej Sapkowski",
+    gender: "male",
+    country: "Poland",
+    pages: 348,
+    published: 1992,
+    category: "fiction",
+    genre: "fantasy",
+    link: "https://www.goodreads.com/book/show/7570539-me-osudu"
+  },
+  {
+    id: 5,
+    title: "The King of Attolia",
+    author: "Megan Whalen Turner",
+    gender: "female",
+    country: "USA",
+    pages: 387,
+    published: 2006,
+    category: "fiction",
+    genre: "fantasy",
+    link: "https://www.goodreads.com/book/show/40159.The_King_of_Attolia"
+  },
+];
+
+export default booksData;
---
+++ src/data/readersData.js
@@ -0,0 +1,29 @@
+const readersData = [
+  {
+    id: 1,
+    name: 'Janka',
+    books: [1, 2, 3]
+  },
+  {
+    id: 2,
+    name: 'Danka',
+    books: [2, 3, 4]
+  },
+  {
+    id: 3,
+    name: 'Blanka',
+    books: [1, 2, 4]
+  },
+  {
+    id: 4,
+    name: 'Manka',
+    books: [1, 3, 4]
+  },
+  {
+    id: 5,
+    name: 'Fanka',
+    books: [3, 4, 5]
+  },
+];
+
+export default readersData;
